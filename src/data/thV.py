03f1b7c8f3b6a7bd1816f5feeee84b93aec3a5ac
pea <pea040119@gmail.com>
2023-06-26 16:05:36 +0900

    .

--- src/data/thV.py
+++ src/data/thV.py
@@ -1,24 +1,25 @@
-import numpy as np
-import csv
-import os
-import pandas as pd
+import os, sys, csv, shutil
+sys.path.append("/mnt/e/Program/Python/Colorization")
 
-f = open("results_TM.csv","r")
-reader = csv.reader(f)
-ccnt = 0
-gcnt = 0
-for row in reader:
-    if row[0] == 'label':
-        continue
-    if row[3] == '0':
-        cstr = row[0] + '_4.png'
-        gstr = row[0] + '_2.jpg'
-        cfd = 'data1_TM2_Threshold/Color/' + cstr
-        gfd = 'data1_TM2_Threshold/Gray/' + gstr
-        if os.path.isfile(gfd):
-            os.remove(gfd)
-            gcnt += 1
-        if os.path.isfile(cfd):
-            os.remove(cfd)
-            ccnt += 1
-print(ccnt,gcnt)
+
+
+def cut_thV(GT_dir, imgs_dir, output_file):
+    if not(os.path.exists(imgs_dir)):
+        os.makedirs(imgs_dir)
+    n = 0
+    with open(output_file, "r", newline="") as csv_file:
+        reader = csv.reader(csv_file)
+        next(reader)
+        for line in reader:
+            if not(line[3]==0):
+                n+=1
+                print(line[0])
+                shutil.copy(os.path.join(GT_dir, line[0]+'_4.png'), os.path.join(imgs_dir, line[0][:-5]+'_4.png'))
+    print(n)
+
+
+if __name__=='__main__':
+    GT_dir = 'dataset/TM2/Color'
+    imgs_dir = 'dataset/TM2_THV/Color'
+    output_file = "result/_csv/results_TM2.csv"
+    cut_thV(GT_dir, imgs_dir, output_file)
